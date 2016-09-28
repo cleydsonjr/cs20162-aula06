0fb771bce609ca42ec5473c890c76ffd93423d8e
Cleydson Júnior <cleydsonjr@gmail.com>
2016-09-28 20:21:40 -0300

    Testes de aceitação para a implementação do algoritimo de verificação usando soma

---
+++ src/main/java/br/ufg/cs/cleydsonjr/VerificadorCPF.java
@@ -0,0 +1,12 @@
+package br.ufg.cs.cleydsonjr;
+
+/**
+ * Classe auxiliar contendo métodos para verificação de validade de um CPF fornecido.
+ */
+public class VerificadorCPF {
+
+    public boolean verifiqueUsandoSoma(int[] cpfInformado) {
+        return false;
+    }
+
+}
