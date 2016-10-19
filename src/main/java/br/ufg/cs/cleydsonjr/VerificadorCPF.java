c237b969915e65ee6c2af641264f33577ecb677d
Cleydson Júnior <cleydsonjr@gmail.com>
2016-10-19 20:11:20 -0200

    Adicionado plugin do checkstyle e correção das violações

--- src/main/java/br/ufg/cs/cleydsonjr/VerificadorCPF.java
+++ src/main/java/br/ufg/cs/cleydsonjr/VerificadorCPF.java
@@ -4,41 +4,117 @@ package br.ufg.cs.cleydsonjr;
  * Classe auxiliar contendo métodos para verificação de validade de um CPF fornecido.
  */
 public class VerificadorCPF {
+
+    /**
+     * Representação do tamanho padrão de um CPF.
+     */
     private static final int TAMANHO_CPF = 11;
 
     /**
-     * Verifica a validade de um CPF fornecido usando um algoritimo baseado em soma simples dos digitos
+     * Representação do número Zero.
+     */
+    private static final int ZERO = 0;
+
+    /**
+     * Representação do número Um.
+     */
+    private static final int UM = 1;
+
+    /**
+     * Representação do número Dois.
+     */
+    private static final int DOIS = 2;
+
+    /**
+     * Representação do número Três.
+     */
+    private static final int TRES = 3;
+
+    /**
+     * Representação do número Quatro.
+     */
+    private static final int QUATRO = 4;
+
+    /**
+     * Representação do número Cinco.
+     */
+    private static final int CINCO = 5;
+
+    /**
+     * Representação do número Seis.
+     */
+    private static final int SEIS = 6;
+
+    /**
+     * Representação do número Zero.
+     */
+    private static final int SETE = 7;
+
+    /**
+     * Representação do número Zero.
+     */
+    private static final int OITO = 8;
+
+    /**
+     * Representação do número Nove.
+     */
+    private static final int NOVE = 9;
+
+    /**
+     * Representação do número Dez.
+     */
+    private static final int DEZ = 10;
+
+    /**
+     * Verifica a validade de um CPF fornecido usando um algoritimo baseado em soma simples dos digitos.
      * Equivalente ao "Algoritimo 22" da especificação da atividade: ´CPF(d[])´
      *
      * @param cpfInformado Array de inteiros contendo os 11 digitos do CPF
      * @return O resultado da verificação se o CPF informado é válido
      */
-    public boolean verifiqueUsandoSomaSimples(int[] cpfInformado) {
+    public final boolean verifiqueUsandoSomaSimples(final int[] cpfInformado) {
         // Verificando se o Array fornecido tem o tamanho correto
         valideArrayFornecido(cpfInformado);
 
         // Extraindo digitos do Array
-        int digito1 = cpfInformado[0];
-        int digito2 = cpfInformado[1];
-        int digito3 = cpfInformado[2];
-        int digito4 = cpfInformado[3];
-        int digito5 = cpfInformado[4];
-        int digito6 = cpfInformado[5];
-        int digito7 = cpfInformado[6];
-        int digito8 = cpfInformado[7];
-        int digito9 = cpfInformado[8];
-        int digito10 = cpfInformado[9];
-        int digito11 = cpfInformado[10];
+        int digito1 = cpfInformado[ZERO];
+        int digito2 = cpfInformado[UM];
+        int digito3 = cpfInformado[DOIS];
+        int digito4 = cpfInformado[TRES];
+        int digito5 = cpfInformado[QUATRO];
+        int digito6 = cpfInformado[CINCO];
+        int digito7 = cpfInformado[SEIS];
+        int digito8 = cpfInformado[SETE];
+        int digito9 = cpfInformado[OITO];
+        int digito10 = cpfInformado[NOVE];
+        int digito11 = cpfInformado[DEZ];
 
-        // Somando primeros 9 digitos multiplicando os pesos
-        int primeiraSoma = digito1 + 2 * digito2 + 3 * digito3 + 4 * digito4 + 5 * digito5 + 6 * digito6 + 7 * digito7 + 8 * digito8 + 9 * digito9;
-        // Somando digitos 2 a 10 multiplicando os pesos
-        int segundaSoma = digito2 + 2 * digito3 + 3 * digito4 + 4 * digito5 + 5 * digito6 + 6 * digito7 + 7 * digito8 + 8 * digito9 + 9 * digito10;
+        // Somando primeros NOVE digitos multiplicando os pesos
+        int primeiraSoma = digito1;
+        primeiraSoma += DOIS * digito2;
+        primeiraSoma += TRES * digito3;
+        primeiraSoma += QUATRO * digito4;
+        primeiraSoma += CINCO * digito5;
+        primeiraSoma += SEIS * digito6;
+        primeiraSoma += SETE * digito7;
+        primeiraSoma += OITO * digito8;
+        primeiraSoma += NOVE * digito9;
+
+        // Somando digitos 2 a DEZ multiplicando os pesos
+        int segundaSoma = digito2;
+        segundaSoma += DOIS * digito3;
+        segundaSoma += TRES * digito4;
+        segundaSoma += QUATRO * digito5;
+        segundaSoma += CINCO * digito6;
+        segundaSoma += SEIS * digito7;
+        segundaSoma += SETE * digito8;
+        segundaSoma += OITO * digito9;
+        segundaSoma += NOVE * digito10;
 
         // Obtendo primeiro digito verificador
-        int digitoVerificador1 = (primeiraSoma % 11) % 10;
+        int digitoVerificador1 = (primeiraSoma % TAMANHO_CPF) % DEZ;
         // Obtendo segundo digito verificador
-        int digitoVerificador2 = (segundaSoma % 11) % 10;
+        int digitoVerificador2 = (segundaSoma % TAMANHO_CPF) % DEZ;
 
         // Comparando digitos calculados com os fornecidos
         return digitoVerificador1 == digito10 && digitoVerificador2 == digito11;
@@ -52,78 +128,86 @@ public class VerificadorCPF {
      * @param cpfInformado Array de inteiros contendo os 11 digitos do CPF
      * @return O resultado da verificação se o CPF informado é válido
      */
-    public boolean verifiqueUsandoLoop(int[] cpfInformado) {
+    public final boolean verifiqueUsandoLoop(final int[] cpfInformado) {
         // Verificando se o Array fornecido tem o tamanho correto
         valideArrayFornecido(cpfInformado);
 
-        int digito10 = cpfInformado[9];
-        int digito11 = cpfInformado[10];
+        int digito10 = cpfInformado[NOVE];
+        int digito11 = cpfInformado[DEZ];
 
-        int primeiraSoma = cpfInformado[8];
-        int segundaSoma = cpfInformado[8];
+        int primeiraSoma = cpfInformado[OITO];
+        int segundaSoma = cpfInformado[OITO];
 
         // Iterando sobre digitos para obter as somas
-        for (int i = 7; i >= 0; i--) {
+        for (int i = SETE; i >= ZERO; i--) {
             primeiraSoma += cpfInformado[i];
             segundaSoma += primeiraSoma;
         }
 
         // Obtendo primeiro digito verificador
-        int digitoVerificador1 = (segundaSoma % 11) % 10;
+        int digitoVerificador1 = (segundaSoma % TAMANHO_CPF) % DEZ;
 
         // Obtendo segundo digito verificador
-        int auxiliar = segundaSoma - primeiraSoma + 9 * digitoVerificador1;
-        int digitoVerificador2 = (auxiliar % 11) % 10;
+        int auxiliar = segundaSoma - primeiraSoma + NOVE * digitoVerificador1;
+        int digitoVerificador2 = (auxiliar % TAMANHO_CPF) % DEZ;
 
         // Comparando digitos calculados com os fornecidos
         return digitoVerificador1 == digito10 && digitoVerificador2 == digito11;
     }
 
     /**
-     * Verifica a validade de um CPF fornecido usando uma variação do algoritimo "verifiqueUsandoLoop", removendo o Loop
+     * Verifica a validade de um CPF fornecido usando variação do algoritimo "verifiqueUsandoLoop", removendo o Loop.
      *
      * @param cpfInformado Array de inteiros contendo os 11 digitos do CPF
      * @return O resultado da verificação se o CPF informado é válido
      */
-    public boolean verifiqueUsandoLoopDesenvolvido(int[] cpfInformado) {
+    public final boolean verifiqueUsandoLoopDesenvolvido(final int[] cpfInformado) {
         // Verificando se o Array fornecido tem o tamanho correto
         valideArrayFornecido(cpfInformado);
 
         // Extraindo digitos do Array
-        int digito1 = cpfInformado[0];
-        int digito2 = cpfInformado[1];
-        int digito3 = cpfInformado[2];
-        int digito4 = cpfInformado[3];
-        int digito5 = cpfInformado[4];
-        int digito6 = cpfInformado[5];
-        int digito7 = cpfInformado[6];
-        int digito8 = cpfInformado[7];
-        int digito9 = cpfInformado[8];
-        int digito10 = cpfInformado[9];
-        int digito11 = cpfInformado[10];
+        int digito1 = cpfInformado[ZERO];
+        int digito2 = cpfInformado[UM];
+        int digito3 = cpfInformado[DOIS];
+        int digito4 = cpfInformado[TRES];
+        int digito5 = cpfInformado[QUATRO];
+        int digito6 = cpfInformado[CINCO];
+        int digito7 = cpfInformado[SEIS];
+        int digito8 = cpfInformado[SETE];
+        int digito9 = cpfInformado[OITO];
+        int digito10 = cpfInformado[NOVE];
+        int digito11 = cpfInformado[DEZ];
 
         // Obtendo primeira soma
         int primeiraSoma = digito9 + digito8 + digito7 + digito6 + digito5 + digito4 + digito3 + digito2 + digito1;
         // Obtendo segunda soma
-        int segundaSoma = digito9 * 9 + digito8 * 8 + digito7 * 7 + digito6 * 6 + digito5 * 5 + digito4 * 4 + digito3 * 3 + digito2 * 2 + digito1;
+        int segundaSoma = digito9;
+        segundaSoma = segundaSoma * NOVE + digito8;
+        segundaSoma = segundaSoma * OITO + digito7;
+        segundaSoma = segundaSoma * SETE + digito6;
+        segundaSoma = segundaSoma * SEIS + digito5;
+        segundaSoma = segundaSoma * CINCO + digito4;
+        segundaSoma = segundaSoma * QUATRO + digito3;
+        segundaSoma = segundaSoma * TRES + digito2;
+        segundaSoma = segundaSoma * DOIS + digito1;
 
         // Obtendo primeiro digito verificador
-        int digitoVerificador1 = (segundaSoma % 11) % 10;
+        int digitoVerificador1 = (segundaSoma % TAMANHO_CPF) % DEZ;
 
         // Obtendo segundo digito verificador
-        int auxiliar = segundaSoma - primeiraSoma + 9 * digitoVerificador1;
-        int digitoVerificador2 = (auxiliar % 11) % 10;
+        int auxiliar = segundaSoma - primeiraSoma + NOVE * digitoVerificador1;
+        int digitoVerificador2 = (auxiliar % TAMANHO_CPF) % DEZ;
 
         // Comparando digitos calculados com os fornecidos
         return digitoVerificador1 == digito10 && digitoVerificador2 == digito11;
     }
 
     /**
-     * Metodo auxiliar para validação simples do array com os digitos de um CPF
+     * Metodo auxiliar para validação simples do array com os digitos de um CPF.
      *
      * @param cpfInformado Array com CPF para validar
      */
-    private void valideArrayFornecido(int[] cpfInformado) {
+    private void valideArrayFornecido(final int[] cpfInformado) {
         // Array deve ter 11 digitos
         if (cpfInformado.length != TAMANHO_CPF) {
             throw new IllegalArgumentException("CPF com número incorreto de dígitos fornecido");
---
+++ src/main/java/br/ufg/cs/cleydsonjr/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do repositório de "utilitário". Contém Uma classe
+ * apenas: (a) {@link br.ufg.cs.cleydsonjr.VerificadorCPF}
+ *
+ * @see br.ufg.cs.cleydsonjr.VerificadorCPF
+ */
+package br.ufg.cs.cleydsonjr;
